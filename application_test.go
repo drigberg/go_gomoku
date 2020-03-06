0b4da47e7b83b1b59e0d27374c3ba01a5fb8c6fb
Daniel Rigberg <daniel@lonerooftop.com>
2020-03-06 16:50:21 +0100

    best tests ever, full e2e with text input

--- application_test.go
+++ application_test.go
@@ -1,16 +1,54 @@
 package main
 
 import (
-	"strings"
+	"bytes"
+	"errors"
 	"testing"
 	"time"
 
 	"go_gomoku/client"
 	"go_gomoku/constants"
 	"go_gomoku/server"
+	"go_gomoku/types"
 )
 
-func TestGoGomokuCreateGame(t *testing.T) {
+type incrementalReader struct {
+	input 	chan string
+}
+
+func (reader incrementalReader) Read(p []byte) (n int, err error) {
+	str := <- reader.input
+	buf := new(bytes.Buffer)
+	buf.Write([]byte(str))
+	n, err = buf.Read(p)
+	return
+}
+
+func waitForHandledRequest(newClient *client.Client, action string) (types.Request, error) {
+	var err error
+	var request types.Request
+	done := make(chan types.Request)
+
+	go func() {
+		for {
+			request := <- newClient.HandledRequests
+			if request.Action == action {
+				done <- request
+				return
+			}
+		}
+	}()
+
+	select {
+	case r := <-done:
+		request = r
+	case <-time.After(1 * time.Second):
+		err = errors.New("Unable to connect and create game")
+	}
+	return request, err
+}
+
+func TestGoGomokuConnectSuccess(t *testing.T) {
 	newServer := server.New()
 	go newServer.Listen("3003")
 	defer newServer.Stop()
@@ -28,31 +66,46 @@ func TestGoGomokuCreateGame(t *testing.T) {
 		t.Error("Could not connect")
 	}
 
-	reader := strings.NewReader("mk")
-	newClient.ListenForInput(reader)
-	done := make(chan bool)
-	go func() {
-		for {
-			request := <- newClient.HandledRequests
-			if request.Action == constants.CREATE {
-				done <- true
-				return
-			}
-		}
-	}()
-	select {
-	case <-done:
-	case <-time.After(2 * time.Second):
-		t.Error("Unable to connect and create game")
+	request, err := waitForHandledRequest(&newClient, constants.HOME)
+	if err != nil {
+		t.Fatal(err)
 	}
+	if len(request.Home) != 0 {
+		t.Errorf("Expected there to be no active games, found %d", len(request.Home))
+	}
+}
+
+func TestGoGomokuCreateGameSuccess(t *testing.T) {
+	newServer := server.New()
+	go newServer.Listen("3003")
+	defer newServer.Stop()
+
+	newClient := client.New("Test")
+	newClient.DisablePrint = true
+	socketClient := newClient.Connect("localhost", "3003")
+	defer socketClient.Socket.Close()
+
+	connected := make(chan bool)
+	go socketClient.Receive(newClient.Handler, &connected)
+	select {
+	case <-connected:
+	case <-time.After(5 * time.Second):
+		t.Error("Could not connect")
+	}
+	waitForHandledRequest(&newClient, constants.HOME)
+
+	reader := incrementalReader{make(chan string)}
+	go func() {newClient.ListenForInput(reader)}()
+
+	reader.input <- "mk\n"
+	waitForHandledRequest(&newClient, constants.CREATE)
 	
 	if newClient.GameID == -1 {
 		t.Errorf("Client still has gameID -1")
 	}
 }
 
-
-func TestGoGomokuCreateGame2(t *testing.T) {
+func TestGoGomokuHomeFromGameSuccess(t *testing.T) {
 	newServer := server.New()
 	go newServer.Listen("3003")
 	defer newServer.Stop()
@@ -70,25 +123,21 @@ func TestGoGomokuCreateGame2(t *testing.T) {
 		t.Error("Could not connect")
 	}
 
-	reader := strings.NewReader("mk")
-	newClient.ListenForInput(reader)
-	done := make(chan bool)
-	go func() {
-		for {
-			request := <- newClient.HandledRequests
-			if request.Action == constants.CREATE {
-				done <- true
-				return
-			}
-		}
-	}()
-	select {
-	case <-done:
-	case <-time.After(2 * time.Second):
-		t.Error("Unable to connect and create game")
+	waitForHandledRequest(&newClient, constants.HOME)
+
+	reader := incrementalReader{make(chan string)}
+	go func() {newClient.ListenForInput(reader)}()
+
+	reader.input <- "mk\n"
+	waitForHandledRequest(&newClient, constants.CREATE)
+
+	reader.input <- "hm\n"
+	reader.input <- "y\n"
+	request, err := waitForHandledRequest(&newClient, constants.HOME)
+	if err != nil {
+		t.Fatal(err)
 	}
-	
-	if newClient.GameID == -1 {
-		t.Errorf("Client still has gameID -1")
-	}
+	if len(request.Home) != 1 {
+		t.Errorf("Expected there to be 1 active game, found %d", len(request.Home))
+	}	
 }
